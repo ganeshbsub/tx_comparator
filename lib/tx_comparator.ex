b4b8a4e65014678b9903402dfd9a4b9e31dc0c42
Ganesh Bala Subramanian <ganesh.bala@simplabs.com>
2017-03-09 16:01:57 +0100

    Write module skeleton

--- lib/tx_comparator.ex
+++ lib/tx_comparator.ex
@@ -1,3 +1,25 @@
 defmodule TxComparator do
-  
+  def compare(string1, string2, synonyms, words_to_skip) do
+    token_list_1 = 
+      string1
+      |> tokenizer
+      |> analyzer(synonyms, words_to_skip)
+    token_list_2 = 
+      string1
+      |> tokenizer
+      |> analyzer(synonyms, words_to_skip)
+    comparator(token_list_1, token_list_2)
+  end
+
+  defp tokenizer(string) do
+    
+  end
+
+  defp analyzer(string, synonyms, words_to_skip) do
+    
+  end
+
+  defp comparator(token_list_1, token_list_2) do
+    
+  end
 end
