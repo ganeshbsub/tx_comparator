50b0c8e08b8a1ddf80e48fe925ab65452b1f4c31
Ganesh Bala Subramanian <ganesh.bala@simplabs.com>
2017-03-10 14:21:11 +0100

    Finish up module and add doc

--- lib/tx_comparator.ex
+++ lib/tx_comparator.ex
@@ -1,4 +1,8 @@
 defmodule TxComparator do
+  @doc """
+  compare(string1, string2, synonyms_map, words_to_skip_list) tokenises both strings, removes unnecessary words,
+  replaces tokens with synonyms where possible and provides a score based on the comparison of the final list of tokens
+  """
   def compare(string1, string2, synonyms, words_to_skip) do
     token_list_1 = 
       string1
@@ -9,7 +13,7 @@ defmodule TxComparator do
       |> MapSet.new
 
     token_list_2 = 
-      string1
+      string2
       |> String.downcase
       |> tokeniser
       |> remove_words(words_to_skip)
@@ -19,12 +23,22 @@ defmodule TxComparator do
     comparator(token_list_1, token_list_2)
   end
 
-  defp tokeniser(string), do: String.split(string, ~r/[^\p{L}'-]/u, trim: true)
+  @doc """
+  tokeniser(string) tokenises a string and returns a list of tokens
+  """
+  def tokeniser(string), do: String.split(string, ~r/[^\p{L}'-]/u, trim: true)
 
-  defp remove_words(tokens, words_to_skip), do: Enum.reject(tokens, fn(token) -> Enum.member?(words_to_skip, token) end)
+  @doc """
+  remove_words(token_list, words_to_skip_list) removes unnecesaary tokens from token_list using the words_to_skip_list
+  """
+  def remove_words(tokens, words_to_skip), do: Enum.reject(tokens, fn(token) -> Enum.member?(words_to_skip, token) end)
 
-  defp generalise(tokens, synonyms) do
-    Enum.reduce(tokens, 
+  @doc """
+  generalise(token_list, synonym_map) replaces tokens in token_list using synonym_map where possible.
+  synonym map should be in the following format: %{"token" => "synonym"}. Please avoid colon-notation for the key-value pairs.
+  """
+  def generalise(tokens, synonyms) do
+    Enum.reduce(tokens, [],
       fn(token, new_tokens) -> 
         case Map.get(synonyms, token) do
           nil -> new_tokens ++ [token]
@@ -34,7 +48,24 @@ defmodule TxComparator do
     )
   end
 
-  defp comparator(token_list_1, token_list_2) do
-    
+  @doc """
+  comparator(token_mapset_1, token_mapset_2) compares two mapsets and returns a score between 0 and 1 based on how much they match.
+  """
+  def comparator(token_list_1, token_list_2) do
+      cond do 
+        MapSet.equal?(token_list_1, token_list_2) ->
+          1.0
+        MapSet.disjoint?(token_list_1, token_list_2) ->
+          0.0
+        true ->
+          common_tokens = MapSet.intersection(token_list_1, token_list_2)
+          sizeof_token_list_1 = MapSet.size(token_list_1)
+          sizeof_token_list_2 = MapSet.size(token_list_2)
+          if sizeof_token_list_1 >= sizeof_token_list_2 do
+            MapSet.size(common_tokens)/sizeof_token_list_1
+          else
+            MapSet.size(common_tokens)/sizeof_token_list_2
+          end
+      end
   end
 end
